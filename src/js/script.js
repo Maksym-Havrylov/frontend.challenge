91309f00f1a4ec88f55b0ec7a4fe89e4ee73a962
Maksym Havrylov <salomondr007@gmail.com>
2021-04-19 18:31:08 +0300

    FAQ is ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,26 @@
 @@include('_testWebP.js');
 @@include('vendor.js');
 
+// .burger
 $(document).ready(function () {
   $('.icon-menu').click(function(event) {
     $('.icon-menu,.menu__body').toggleClass('_active');
     $('body').toggleClass('lock');
   });
 });
+
+// .toggle (accordion)
+var acc = document.getElementsByClassName("faq__accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
