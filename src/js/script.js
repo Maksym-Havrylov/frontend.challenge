c11d28a675de9a9f53fdaede75e1e9f3d45489d4
Maksym Havrylov <salomondr007@gmail.com>
2021-04-23 21:16:54 +0300

    calendar

--- src/js/script.js
+++ src/js/script.js
@@ -86,22 +86,5 @@ $('.plus-btn').on('click', function (e) {
 }); /* --- quantity ends ---*/
 
 /* --- calendar datepicker ---*/
-$(function() {
-  $( ".calendar" ).datepicker({
-		dateFormat: 'dd/mm/yy',
-		firstDay: 1
-	});
-	
-	$(document).on('click', '.date-picker .input', function(e){
-		var $me = $(this),
-				$parent = $me.parents('.date-picker');
-		$parent.toggleClass('open');
-	});
-	
-	$(".calendar").on("change",function(){
-		var $me = $(this),
-				$selected = $me.val(),
-				$parent = $me.parents('.date-picker');
-		$parent.find('.result').children('span').html($selected);
-	});
-}); /* --- calendar datepicker ends ---*/
+
+ /* --- calendar datepicker ends ---*/
