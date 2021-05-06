f2b0cb06f1d098dcc63881cb3d792607599ca4d6
Maksym Havrylov <salomondr007@gmail.com>
2021-05-06 10:21:35 +0300

    $md1-6 i-mac,watch,calculator

--- src/js/script.js
+++ src/js/script.js
@@ -39,13 +39,19 @@ $(document).ready(function () {
     autoplay: true,
     // dots: true,
     responsive: [{
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
         breakpoint: 768,
         settings: {
           slidesToShow: 2,
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 576,
         settings: {
           slidesToShow: 1,
         }
